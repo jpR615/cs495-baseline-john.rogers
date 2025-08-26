60ebcd7fa5e31e155c29285e6056eaa0512e2039
J.P. Rogers <jprogers2023@gmail.com>
2025-08-26 16:35:12 -0400

    Rework unit tests to use unittest instead of pytest

--- .venv/test_utils.py
+++ .venv/test_utils.py
@@ -1,8 +1,15 @@
-def test_lowercaseString():
-    assert utils.normalize("CarrOt") == "carrot"
+import unittest
+import utils
 
-def test_dashString():
-    assert utils.normalize("young sheldon") == "young-sheldon"
+class TestUtils(unittest.TestCase):
+    def test_lowercaseString(self):
+        self.assertEqual(utils.normalize("CarrOt"), "carrot")
 
-def test_lowerCaseString_and_dashString():
-    assert utils.normalize("Apples and Bananas") == "apples-and-bananas"
+    def test_dashString(self):
+        self.assertEqual(utils.normalize("young sheldon"), "young-sheldon")
+
+    def test_lowerCaseString_and_dashString(self):
+        self.assertEqual(utils.normalize("Apples and Bananas"), "apples-and-bananas")
+
+if __name__ == '__main__':
+    unittest.main()
